d99fb87a8430d234f09371a8bb1bb99167020403
unknown <asadbd2014@gmail.com>
2024-03-07 11:07:23 +0600

    playRound() implemented

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
-console.log("Hello World !");
+function getComputerChoice(){
+    const choice = ["Rock", "Paper", "Scissors"]
+    const final = choice[Math.floor(Math.random() * choice.length)];
+    return final.toLowerCase();
+}
+
+function playRound(playerSelection, computerSelection){
+    var results = "";
+    if (playerSelection === computerSelection){
+        results = "TIE !";
+    } else if(playerSelection === "rock" && computerSelection !== 'paper'){
+        results = "You Win! " + playerSelection + " beats " + computerSelection;
+    } else if(playerSelection === "paper" && computerSelection !== 'scissors'){
+        results = "You Win! " + playerSelection + " beats " + computerSelection;
+    } else if(playerSelection === "scissors" && computerSelection !== 'rock'){
+        results = "You Win! " + playerSelection + " beats " + computerSelection;
+    }
+    else {
+        results = "You Lose! " + computerSelection + " beats " + playerSelection;
+    }
+    return results;
+}
+
+const playerSelection = prompt("Please enter your choice (rock, paper, scissors)").toLowerCase();
+
+const computerSelection = getComputerChoice();
+console.log("Player: " + playerSelection + " Computer: " + computerSelection);
+console.log(playRound(playerSelection, computerSelection));
